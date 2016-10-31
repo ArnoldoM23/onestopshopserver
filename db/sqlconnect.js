0d292d67281ae72063a151b4d23260867402e1da
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-10-31 01:38:11 -0700

    added sql schema removing mongo

--- db/sqlconnect.js
+++ db/sqlconnect.js
@@ -1,5 +1,10 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('postgres://icgmexbycqrtvl:mJPu5TXKxFBDt5vMBbSB_pQg5d@ec2-54-243-126-40.compute-1.amazonaws.com:5432/d53r4g7ejf87na');
+const sequelize = new Sequelize('onestop', 'young', '', {
+  host: 'localhost',
+  dialect: 'postgres',
+  port: 5432
+
+});
 
 module.exports = sequelize;
