0e15d1f37d7382d2b16b9dec8cdc2f63ea95c1ae
AnorldoM23 <arnoldomunoz23@yahoo.com>
2016-12-19 05:12:17 -0800

    minor fix

--- routes/routes.js
+++ routes/routes.js
@@ -10,7 +10,7 @@ const requireAuth = passport.authenticate('jwt', {session: false});
 const requireSignin = passport.authenticate('local', {session: false});
 
 // const ROOT_URL = 'http://localhost:3000';
-const ROOT_URL = 'http://shoponceserver.herokuapp.com';
+const ROOT_URL = 'http://shoponce.herokuapp.com';
 
 
 function tokenForUser(user){
